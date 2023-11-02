e89c52202244cbc6cd6ad6edb29c7c5ceed02165
verafied <verasolnygren@gmail.com>
2023-11-02 17:43:59 +0100

    Starting to implement incorporating local music into library

---
+++ MusicLib/src/FileExtension.java
@@ -0,0 +1,6 @@
+/**
+ * Music file extension the program knows how to handle
+ */
+public enum FileExtension {
+    MP3, M4A, FLAC, MP4, WAV, WMA, AAC
+}
---
+++ MusicLib/src/Metadata.java
@@ -0,0 +1,70 @@
+public class Metadata {
+    String tag;
+    String songname;
+    String artist;
+    String album;
+    short year;
+    String comment;
+    String genre;
+
+    public Metadata(String songPath, FileExtension type){
+        //depending on which type, open it differently
+    }
+
+    public String getTag() {
+        return this.tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getSongname() {
+        return this.songname;
+    }
+
+    public void setSongName(String songName) {
+        this.songname = songName;
+    }
+
+    public String getArtist() {
+        return this.artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return this.album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public short getYear() {
+        return this.year;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+}
