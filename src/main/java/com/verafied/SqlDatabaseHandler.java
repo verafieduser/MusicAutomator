d53f33c366e5ac88ed72206d98547c1028f05075
verafied <verasolnygren@gmail.com>
2023-11-13 19:42:20 +0100

    Updated gitignore

--- src/main/java/com/verafied/SqlDatabaseHandler.java
+++ src/main/java/com/verafied/SqlDatabaseHandler.java
@@ -1,11 +1,9 @@
 package com.verafied;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-//TODO: implement
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -23,7 +21,7 @@ public class SqlDatabaseHandler {
     try {
       connection = DriverManager.getConnection("jdbc:sqlite:" + url + "/demo.db");
       initialize();
-    } catch (SQLException | IOException e) {
+    } catch (SQLException e) {
       // if the error message is "out of memory",
       // it probably means no database file is found
       e.printStackTrace();
@@ -38,7 +36,7 @@ public class SqlDatabaseHandler {
     }
   }
 
-  public boolean initialize() throws IOException {
+  public boolean initialize() {
     try (Statement statement = connection.createStatement()) {
       statement.setQueryTimeout(30); // set timeout to 30 sec.
       statement.executeUpdate("DROP TABLE IF EXISTS artist");
@@ -65,8 +63,6 @@ public class SqlDatabaseHandler {
   public boolean update(Song song) {
     try (Statement statement = connection.createStatement()) {
       statement.setQueryTimeout(30); // set timeout to 30 sec.
-      statement.executeUpdate("insert into person values(1, 'leo')");
-      statement.executeUpdate("insert into person values(2, 'yui')");
     } catch (SQLException e) {
       e.printStackTrace();
     }
