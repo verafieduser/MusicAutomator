0dec96093b3b99905f3477d5f0f7a6f5e65899ba
verafied <verasolnygren@gmail.com>
2023-11-27 19:38:41 +0100

    Added .net stump

---
+++ .NET/MusicAutomator/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Soulseek;
+
+public class SoulseekClientWrapper
+{
+    static public void Main ()
+    {
+        var Try = new SoulseekClientWrapper();
+        Try.Test();
+
+        
+    }
+
+    async void Test(){
+        var client = new SoulseekClient();
+        await client.ConnectAsync("Username", "Password");
+        
+        Console.WriteLine(client.ToString());
+    }
+
+}
